f827dc3b278835b2cb138c7c872e2196cafd13db
Laurens De Vries <home@Laurenss-Mac-mini.local>
2022-01-18 19:51:43 +0100

    added products to customer model

--- jello/models.py
+++ jello/models.py
@@ -26,8 +26,9 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-    class Product(models.Model):
-        name = models.CharField(max_length=50, unique=True)
 
-        def __str__(self):
-            return f"Productname = {self.name}"
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f"Productname = {self.name}"
