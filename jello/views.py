e5cfad999ec11e008670987ad53d035a2d376db9
Laurens De Vries <home@Laurenss-Mac-mini.local>
2022-01-19 12:49:49 +0100

    added very simple search functionality

--- jello/views.py
+++ jello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import CustomerForm
+from .models import Customer
 
 # Create your views here.
 
@@ -28,3 +29,28 @@ class CreateCustomer(View):
 class LandingPage(View):
     def get(self, request):
         return render(request, 'jello/landingpage.html')
+
+    def post(self, request):
+        query = request.POST
+        qset = self.qqset(query['query'])
+        has_no_customers = False
+        if qset:
+            qset = qset.values()
+        else:
+            has_no_customers = True
+        return render(request, 'jello/landingpage.html', {
+            "qset": qset,
+            "has_no_customers": has_no_customers
+        })
+
+    def qqset(self, query):
+        qset = Customer.objects.all().values("email", "mobile_number",
+                                             "zipcode", "house_number", "id")
+
+        if qset.filter(email=query):
+            return(qset.filter(email=query))
+        elif qset.filter(mobile_number=query):
+            return(qset.filter(mobile_number=query))
+        elif qset.filter(zipcode=query[:6]):
+            qset = qset.filter(zipcode=query[:6])
+            return(qset.filter(house_number=query[7:]))
