df36c909a9e78002b08799190ed16a8633314c4b
Laurens De Vries <home@Laurenss-Mac-mini.fritz.box>
2022-05-17 09:25:48 +0200

    changed home to / rather than /jello

--- jello/urls.py
+++ jello/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from jello import views
 
 urlpatterns = [
-    path('jello/', views.LandingPage.as_view(), name="landing-page"),
-    path('jello/create-customer/',
+    path('', views.LandingPage.as_view(), name="landing-page"),
+    path('create-customer/',
          views.CreateCustomer.as_view(), name="create-customer"),
-    path('jello/create-product/',
+    path('create-product/',
          views.CreateProduct.as_view(), name="create-product"),
-    path('jello/add-products', views.update_customer_products, name="add-products"),
-    path('jello/<int:id>', views.CustomerDetail.as_view(), name="customer-detail")
+    path('add-products', views.update_customer_products, name="add-products"),
+    path('<int:id>', views.CustomerDetail.as_view(), name="customer-detail")
 ]
